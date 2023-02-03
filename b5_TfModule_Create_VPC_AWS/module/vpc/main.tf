cde3d5cff9877e005f55501b0c38a8c0470110dc
luudinhmac <nguoiemcuanui@gmail.com>
2023-02-03 18:11:14 +0700

    create module vpc

---
+++ b5_TfModule_Create_VPC_AWS/module/vpc/main.tf
@@ -0,0 +1,93 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = true
+
+  tags = {
+    "Name" = "custom VPC"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  count = length(var.private_subnet)
+
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.private_subnet[count.index]
+  availability_zone = var.availability_zone[count.index % length(var.availability_zone)]
+
+  tags = {
+    "Name" = "private-subnet"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  count = length(var.public_subnet)
+
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.public_subnet[count.index]
+  availability_zone = var.availability_zone[count.index % length(var.availability_zone)]
+
+  tags = {
+    "Name" = "public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "my_ig" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    "Name" = "custom ig"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_ig.id
+  }
+
+  tags = {
+    "Name" = "public"
+  }
+}
+
+resource "aws_route_table_association" "public_association" {
+  for_each       = { for i, j in aws_subnet.public_subnet : i => j }
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "public" {
+  depends_on = [aws_internet_gateway.ig]
+
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnet[0].id
+
+  tags = {
+    Name = "Public NAT"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.public.id
+  }
+
+  tags = {
+    "Name" = "private"
+  }
+}
+
+resource "aws_route_table_association" "public_private" {
+  for_each       = { for i, j in aws_subnet.private_subnet : i => j }
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
+}
