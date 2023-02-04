385440df9ba2b53bb135c7b15a2d35a679209d1d
luudinhmac <nguoiemcuanui@gmail.com>
2023-02-04 11:03:06 +0700

    update

--- b6_Module_In_Depth-CreateMultiTierApplication/main.tf
+++ b6_Module_In_Depth-CreateMultiTierApplication/main.tf
@@ -3,31 +3,33 @@ locals {
 }
 
 provider "aws" {
-    region = "us-east-2"  
+  region = "us-east-2"
 }
 
 module "networking" {
-    source = "./modules/networking"
-    
-    project = local.project
-    vpc_cidr = "10.0.0.0/16"
-    private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-    public_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-    database_subnets = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
+  source = "./modules/networking"
+
+  project          = local.project
+  vpc_cidr         = "10.0.0.0/16"
+  private_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets   = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  database_subnets = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
 }
 
-module "autoscaling"{
-    source = "./modules/autoscaling"
-    project = local.project
-    vpc = module.networking.vpc
-    sg = module.networking.sg
-    db_config = module.database.config
+module "autoscaling" {
+  source    = "./modules/autoscaling"
+  project   = local.project
+  vpc       = module.networking.vpc
+  sg        = module.networking.sg
+  db_config = module.database.config
 }
 
 module "database" {
-    source = "./modules/database"
-    vpc = module.networking.vpc
-    sg = module.networking.sg
+  source = "./modules/database"
+
+  project = local.project
+  vpc     = module.networking.vpc
+  sg      = module.networking.sg
 }
 
 
