03f25de974f86a2968b7e19322e16e893b100a07
luudinhmac <nguoiemcuanui@gmail.com>
2023-02-08 20:10:49 +0700

    fix source local module green/blue

--- b11_Terraform-Zero-Downtime-with-blue_green-deployments/local/main.tf
+++ b11_Terraform-Zero-Downtime-with-blue_green-deployments/local/main.tf
@@ -7,12 +7,12 @@ variable "production" {
 }
 
 module "base" {
-  source     = "module/base"
+  source     = "./module/base"
   production = var.production
 }
 
 module "green" {
-  source      = "module/autoscaling"
+  source      = "./module/autoscaling"
   app_version = "v1.0"
   label       = "green"
   base        = module.base
@@ -20,7 +20,7 @@ module "green" {
 
 
 module "blue" {
-  source      = "module/autoscaling"
+  source      = "./module/autoscaling"
   app_version = "v2.0"
   label       = "blue"
   base        = module.base
